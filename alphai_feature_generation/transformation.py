9b89174dd185d299575ef0c716c3f0231143addd
Fergus Simpson <fergus.simpson@alpha-i.co>
2017-12-13 15:11:14 +0000

    Set default local feature to False

--- alphai_feature_generation/transformation.py
+++ alphai_feature_generation/transformation.py
@@ -155,7 +155,7 @@ class FinancialDataTransformation(DataTransformation):
                 self.features_start_market_minute,
                 single_feature_dict['is_target'],
                 self.exchange_calendar,
-                single_feature_dict['local'],
+                single_feature_dict.get('local', False),
                 self.classify_per_series,
                 self.normalise_per_series
             ))
