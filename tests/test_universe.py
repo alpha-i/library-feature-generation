05797961cadb5505389b8c7618afd06d7393d5a5
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2018-01-18 13:17:23 +0000

    end date reintroduced before the loop so that the logs displyed are accurate

--- tests/test_universe.py
+++ tests/test_universe.py
@@ -26,7 +26,7 @@ class TestVolumeUniverseProvider(TestCase):
         self.assertEquals(self._monthly_30_nyse_False_3['dropna'], volume_universe._dropna)
         self.assertEquals(self._monthly_30_nyse_False_3['nassets'], volume_universe._nassets)
 
-    def test_get_historical_universes(self):
+    def test_get_historical_universes_simple_two_year(self):
         volume_universe = VolumeUniverseProvider(self._monthly_30_nyse_False_3)
         rng = pd.date_range(start='20070101', end='20090101', freq='T')
         df_close = pd.DataFrame(index=rng, data=np.abs(np.random.randn(3 * len(rng))).reshape([len(rng), 3]),
@@ -46,4 +46,46 @@ class TestVolumeUniverseProvider(TestCase):
         self.assertEquals(historical_universes['end_date'][0], datetime.date(2007, 3, 1))
         self.assertEquals(historical_universes['end_date'][22], datetime.date(2009, 1, 1))
         self.assertEquals(historical_universes['end_date'][22], rng[-1].to_datetime().date())
-        # print(historical_universes)
+
+    def test_get_historical_universes_two_year_with_offset_at_the_beginning(self):
+        volume_universe = VolumeUniverseProvider(self._monthly_30_nyse_False_3)
+        rng = pd.date_range(start='20061224', end='20090101', freq='T')
+        df_close = pd.DataFrame(index=rng, data=np.abs(np.random.randn(3 * len(rng))).reshape([len(rng), 3]),
+                                columns=['AAPL', 'MSFT', 'GOOGL'])
+        df_volume = pd.DataFrame(index=rng, data=np.ones(3 * len(rng)).reshape([len(rng), 3]),
+                                 columns=['AAPL', 'MSFT', 'GOOGL'])
+
+        data_dict = {
+            'close': df_close,
+            'volume': df_volume
+        }
+
+        historical_universes = volume_universe.get_historical_universes(data_dict)
+        self.assertEquals(historical_universes.shape, (23, 3))
+        self.assertEquals(historical_universes['start_date'][0], datetime.date(2007, 1, 24))
+        self.assertEquals(historical_universes['start_date'][22], datetime.date(2008, 11, 24))
+        self.assertEquals(historical_universes['end_date'][0], datetime.date(2007, 2, 24))
+        self.assertEquals(historical_universes['end_date'][22], datetime.date(2009, 1, 1))
+        self.assertEquals(historical_universes['end_date'][22], rng[-1].to_datetime().date())
+
+    def test_get_historical_universes_two_year_with_offset_at_the_end(self):
+        volume_universe = VolumeUniverseProvider(self._monthly_30_nyse_False_3)
+        rng = pd.date_range(start='20070101', end='20090110', freq='T')
+        df_close = pd.DataFrame(index=rng, data=np.abs(np.random.randn(3 * len(rng))).reshape([len(rng), 3]),
+                                columns=['AAPL', 'MSFT', 'GOOGL'])
+        df_volume = pd.DataFrame(index=rng, data=np.ones(3 * len(rng)).reshape([len(rng), 3]),
+                                 columns=['AAPL', 'MSFT', 'GOOGL'])
+
+        data_dict = {
+            'close': df_close,
+            'volume': df_volume
+        }
+
+        historical_universes = volume_universe.get_historical_universes(data_dict)
+        self.assertEquals(historical_universes.shape, (23, 3))
+        self.assertEquals(historical_universes['start_date'][0], datetime.date(2007, 2, 1))
+        self.assertEquals(historical_universes['start_date'][22], datetime.date(2008, 12, 1))
+        self.assertEquals(historical_universes['end_date'][0], datetime.date(2007, 3, 1))
+        self.assertEquals(historical_universes['end_date'][22], datetime.date(2009, 1, 10))
+        self.assertEquals(historical_universes['end_date'][22], rng[-1].to_datetime().date())
+
