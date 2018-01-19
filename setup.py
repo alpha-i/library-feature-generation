c719d86e92bd14717b4aa348721f6418b8a591e2
Gabriele Alese <gabriele.alese@alpha-i.co>
2018-01-19 18:16:16 +0000

    Version bump to 1.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='alphai_feature_generation',
-    version='1.3.0',
+    version='1.3.1',
     description='Alpha-I Feature Generation',
     author='Christopher Bonnett',
     author_email='christopher.bonnett@alpha-i.co',
