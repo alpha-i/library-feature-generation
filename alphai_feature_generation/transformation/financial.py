09342372eb1e502317b10390fee5ee3562ecc974
Daniele Murroni <daniele.murroni@gmail.com>
2018-03-12 12:22:34 +0000

    some cleaning

--- alphai_feature_generation/transformation/financial.py
+++ alphai_feature_generation/transformation/financial.py
@@ -17,24 +17,8 @@ logger = logging.getLogger(__name__)
 Feature = namedtuple('Feature', 'x_dict y_dict prediction_timestamp target_timestamp')
 
 
-import sys
-import pdb
-
-class ForkedPdb(pdb.Pdb):
-    """A Pdb subclass that may be used
-    from a forked multiprocessing child
-
-    """
-    def interaction(self, *args, **kwargs):
-        _stdin = sys.stdin
-        try:
-            sys.stdin = open('/dev/stdin')
-            pdb.Pdb.interaction(self, *args, **kwargs)
-        finally:
-            sys.stdin = _stdin
-
-
 class FinancialDataTransformation(DataTransformation):
+
     CONFIGURATION_SCHEMA = FinancialDataTransformationConfigurationSchema
 
     def __init__(self, configuration):
